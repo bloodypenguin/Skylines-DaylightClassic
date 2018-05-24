ec1666ca50476257869512888fc338ade1aa99ba
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2018-05-24 13:29:48 +0200

    Add typesafe version of OptionsFramework

--- DaylightClassic/Options.cs
+++ DaylightClassic/Options.cs
@@ -11,39 +11,39 @@ namespace DaylightClassic
         private const string SHORTCUTS = "DC_ACTIONS_SHORTCUTS";
 
         [XmlElement("stockLuts")]
-        [Checkbox("DC_OPTION_LUTS", BASIC, nameof(DaylightClassic), nameof(DaylightClassic.ReplaceLuts))]
+        [Checkbox("DC_OPTION_LUTS", BASIC, typeof(DaylightClassic), nameof(DaylightClassic.ReplaceLuts))]
         public bool StockLuts { set; get; } = true;
 
         [XmlElement("sunlightColor")]
-        [Checkbox("DC_OPTION_SUN_COLOR", BASIC, nameof(DaylightClassic), nameof(DaylightClassic.ReplaceSunlightColor))]
+        [Checkbox("DC_OPTION_SUN_COLOR", BASIC, typeof(DaylightClassic), nameof(DaylightClassic.ReplaceSunlightColor))]
         public bool SunlightColor { set; get; } = true;
 
         [XmlElement("sunlightIntensity")]
-        [Checkbox("DC_OPTION_SUN_INTENSITY", BASIC, nameof(DaylightClassic), nameof(DaylightClassic.ReplaceSunlightIntensity))]
+        [Checkbox("DC_OPTION_SUN_INTENSITY", BASIC, typeof(DaylightClassic), nameof(DaylightClassic.ReplaceSunlightIntensity))]
         public bool SunlightIntensity { set; get; } = true;
 
         [XmlElement("sunPosition")]
-        [Checkbox("DC_OPTION_SUN_POSITION", BASIC, nameof(DaylightClassic), nameof(DaylightClassic.ReplaceLatLong))]
+        [Checkbox("DC_OPTION_SUN_POSITION", BASIC, typeof(DaylightClassic), nameof(DaylightClassic.ReplaceLatLong))]
         public bool SunPosition { set; get; } = true;
 
         [XmlElement("fogEffect")]
-        [Checkbox("DC_OPTION_FOG_EFFECT", ADVANCED, nameof(DaylightClassic), nameof(DaylightClassic.ReplaceFogEffect))]
+        [Checkbox("DC_OPTION_FOG_EFFECT", ADVANCED, typeof(DaylightClassic), nameof(DaylightClassic.ReplaceFogEffect))]
         public bool FogEffect { set; get; } = true;
 
         [XmlElement("fogColor")]
-        [Checkbox("DC_OPTION_FOG_COLOR", ADVANCED, nameof(DaylightClassic), nameof(DaylightClassic.ReplaceFogColor))]
+        [Checkbox("DC_OPTION_FOG_COLOR", ADVANCED, typeof(DaylightClassic), nameof(DaylightClassic.ReplaceFogColor))]
         public bool FogColor { set; get; } = true;
 
         [XmlElement("allowClassicFogEffectIfDayNightIsOn")]
-        [Checkbox("DC_OPTION_CLASSIC_EFFECT_IF_CYCLE_ENABLED", ADVANCED, nameof(DaylightClassic))]
+        [Checkbox("DC_OPTION_CLASSIC_EFFECT_IF_CYCLE_ENABLED", ADVANCED, typeof(DaylightClassic))]
         public bool AllowClassicFogEffectIfDayNightIsOn { set; get; } = true;
 
         [XmlIgnore]
-        [Button("DC_ACTION_TO_CLASSIC", SHORTCUTS, nameof(Mod), nameof(Mod.AllToClassic))]
+        [Button("DC_ACTION_TO_CLASSIC", SHORTCUTS, typeof(Mod), nameof(Mod.AllToClassic))]
         public object AllToClassicButton { set; get; } = null;
 
         [XmlIgnore]
-        [Button("DC_ACTION_TO_AD", SHORTCUTS, nameof(Mod), nameof(Mod.AllToAfterDark))]
+        [Button("DC_ACTION_TO_AD", SHORTCUTS, typeof(Mod), nameof(Mod.AllToAfterDark))]
         public object AllToAffterDarkButton { set; get; } = null;
     }
 }
