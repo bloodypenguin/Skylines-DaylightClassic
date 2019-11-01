5bd41738cf305d3cbb0d362cf53f7b4cf8802f5f
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2019-11-01 02:17:24 +0100

    Make sunsets and sunrises vivid again

--- DaylightClassic/FogColorReplacer.cs
+++ DaylightClassic/FogColorReplacer.cs
@@ -19,11 +19,14 @@ namespace DaylightClassic
         private DayNightProperties _dayNightProperties;
         private Color _cachedSkyTint;
         private Vector3 _cachedWaveLengths;
+        private float _cachedTimeOfDay;
 
         public void Awake()
         {
             _newEffect = FindObjectOfType<DayNightFogEffect>();
             _dayNightProperties = FindObjectOfType<DayNightProperties>();
+            _waveLengthsAd = _dayNightProperties.m_WaveLengths;
+            _skyTintAd = _dayNightProperties.m_SkyTint;
         }
 
         public void Update()
@@ -42,7 +45,8 @@ namespace DaylightClassic
                 _cachedReplaceFogColor == OptionsWrapper<Options>.Options.FogColor &&
                 _cachedEffectEnabled == _newEffect.enabled &&
                 _dayNightProperties.m_WaveLengths.Equals(_cachedWaveLengths) &&
-                _dayNightProperties.m_SkyTint.Equals(_cachedSkyTint))
+                _dayNightProperties.m_SkyTint.Equals(_cachedSkyTint) &&
+                _dayNightProperties.m_TimeOfDay.Equals(_cachedTimeOfDay))
             {
                 return;
             }
@@ -52,38 +56,63 @@ namespace DaylightClassic
             _cachedEffectEnabled = _newEffect.enabled;
             _cachedWaveLengths = _dayNightProperties.m_WaveLengths;
             _cachedSkyTint = _dayNightProperties.m_SkyTint;
+            _cachedTimeOfDay = _dayNightProperties.m_TimeOfDay;
         }
 
         private void ReplaceFogColorImpl(bool toClassic)
         {
-            _dayNightProperties.m_SkyTint = toClassic ? SkyTintClassic : SkyTintAd;
-            _dayNightProperties.m_WaveLengths = toClassic ? WaveLengthsClassic : WaveLengthsAd;
+            _dayNightProperties.m_SkyTint = toClassic
+                ? SkyTintClassicGradient.Evaluate(_dayNightProperties.normalizedTimeOfDay)
+                : _skyTintAd;
+            _dayNightProperties.m_WaveLengths = toClassic
+                ? FromColor(WaveLengthsClassicGradient.Evaluate(_dayNightProperties.normalizedTimeOfDay))
+                : _waveLengthsAd;
         }
 
-        private Color SkyTintAd
+        private Gradient SkyTintClassicGradient => new Gradient()
         {
-            get
+            colorKeys = new GradientColorKey[6]
             {
-                if (_skyTintAd == Color.clear)
-                {
-                    _skyTintAd = _dayNightProperties.m_SkyTint;
-                }
-
-                return _skyTintAd;
+                new GradientColorKey(_skyTintAd, 0f),
+                new GradientColorKey(_skyTintAd, 0.29f),
+                new GradientColorKey(SkyTintClassic, 0.35f),
+                new GradientColorKey(SkyTintClassic, 0.65f),
+                new GradientColorKey(_skyTintAd, 0.71f),
+                new GradientColorKey(_skyTintAd, 1f)
+            },
+            alphaKeys = new GradientAlphaKey[2]
+            {
+                new GradientAlphaKey(1f, 0.0f),
+                new GradientAlphaKey(1f, 1f)
             }
+        };
+
+        private Gradient WaveLengthsClassicGradient => new Gradient()
+        {
+            colorKeys = new GradientColorKey[6]
+            {
+                new GradientColorKey(FromVector3(_waveLengthsAd), 0f),
+                new GradientColorKey(FromVector3(_waveLengthsAd), 0.29f),
+                new GradientColorKey(FromVector3(WaveLengthsClassic), 0.35f),
+                new GradientColorKey(FromVector3(WaveLengthsClassic), 0.65f),
+                new GradientColorKey(FromVector3(_waveLengthsAd), 0.71f),
+                new GradientColorKey(FromVector3(_waveLengthsAd), 1f)
+            },
+            alphaKeys = new GradientAlphaKey[2]
+            {
+                new GradientAlphaKey(1f, 0.0f),
+                new GradientAlphaKey(1f, 1f)
+            }
+        };
+
+        private Color FromVector3(Vector3 vector3)
+        {
+            return new Color(vector3.x / 1000f, vector3.y / 1000f, vector3.z / 1000f);
         }
 
-        private Vector3 WaveLengthsAd
+        private Vector3 FromColor(Color color)
         {
-            get
-            {
-                if (_waveLengthsAd == Vector3.zero)
-                {
-                    _waveLengthsAd = _dayNightProperties.m_WaveLengths;
-                }
-
-                return _waveLengthsAd;
-            }
+            return new Vector3(color.r * 1000f, color.g * 1000f, color.b * 1000f);
         }
     }
 }
