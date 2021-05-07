775042d16e9f4ffbaba02ab9dca1be9f24f7101d
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2021-05-08 01:01:53 +0200

    Bump version to 1.12.2

--- DaylightClassic/Mod.cs
+++ DaylightClassic/Mod.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using ColossalFramework.UI;
 using DaylightClassic.OptionsFramework.Extensions;
 using DaylightClassic.TranslationFramework;
@@ -8,11 +9,13 @@ namespace DaylightClassic
 {
     public class Mod : IUserMod
     {
+        private const string Version = "1.12.2";
+        
         private static UICheckBox[] _checkBoxes;
         private static readonly Translation translation = new Translation();
 
         public string Name => "Daylight Classic";
-        public string Description => translation.GetTranslation("DC_DESCRIPTION");
+        public string Description => $"{translation.GetTranslation("DC_DESCRIPTION")}, version {Version}";
 
         public void OnSettingsUI(UIHelperBase helper)
         {
